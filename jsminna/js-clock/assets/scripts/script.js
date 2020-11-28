f78f3b4c463a856d47d99940f85788b83235028f
Lateef Quadri <Lateef9816@gmail.com>
2020-11-28 17:56:57 +0100

    Made Improvement to the JS Clock app

--- jsminna/js-clock/assets/scripts/script.js
+++ jsminna/js-clock/assets/scripts/script.js
@@ -2,23 +2,39 @@
 const container = document.querySelector(".container");
 const timeEl = document.querySelector(".time");
 const locationEl = document.querySelector("#location");
+const locationEl2 = document.querySelector("#location-2");
 const dayStatus = document.querySelector(".day-status");
 const toggleBtn = document.querySelector(".toggle-btn");
-// 
+
+// /////////
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const date = new Date();
+let now = new Date();
+let onejan = new Date(now.getFullYear(), 0, 1);
+let week = Math.ceil( (((now.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7 );
+
+///////////
 locationEl.innerHTML = `In ${timezone.split("/")[1]}, ${timezone.split("/")[0]}`
+locationEl2.innerHTML = timezone;
+document.querySelector("#day-of-week").innerHTML = new Date().getDay();
+
+document.querySelector("#day-of-the-year").innerHTML = ((week - 2) * 7) + now.getDay();
+
+document.querySelector("#week-number").innerHTML = week;
 
 setInterval(UpdateUI, 1000)
 
 function UpdateUI(){
+    const date = new Date();
     const hour = date.getHours();
-    const minutes = date.getMinutes()
+    const minutes = date.getMinutes();
+
     timeEl.innerHTML = `${hour}:${minutes}`
     container.style.backgroundImage = "url('./assets/images/bg-morning.jpg')";
 
-    if(hour > 11){
+    if(hour > 11 && hour <= 16){
         dayStatus.innerHTML = "afternoon";
+    }else if(hour > 16 && hour < 23 && minutes <= 59 ){
+        dayStatus.innerHTML = "evening";
     }else{
         dayStatus.innerHTML = "morning";
     }
